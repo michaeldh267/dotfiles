0d14bc3e842843b99306599d332b84fd4fae0ee4
Myk <mykyoyo@gmail.com>
2014-04-20 12:19:43 -0700

    update config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,14 +3,21 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.UrgencyHook
 import System.IO
 import XMonad.Layout
 import XMonad.Layout.NoBorders
+import XMonad.StackSet as W (focusUp, focusDown)
+import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
+import XMonad.Actions.CycleWS
+
+
+
 
 main = do
   xmproc <- spawnPipe "/home/myk/.cabal/bin/xmobar /home/myk/.xmobarrc"
 
-  xmonad $ defaultConfig 
+  xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
        { modMask = mod4Mask
        , terminal = "xfce4-terminal"
        , borderWidth = 2
@@ -22,6 +29,15 @@ main = do
                    }
        , startupHook = myStartupHook
        }
+       `additionalKeys` [((mod4Mask, xK_r), spawn "dmenu_run -fn '-xos4-terminus-bold-r-normal-*-16-*-*-*-*-*-*-*' -nb 'Black' -nf 'grey60' -sb 'grey20' -sf 'Gold'")]
+       `additionalKeysP` [ ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 5%+")
+                         , ("<XF86AudioLowerVolume>", spawn "amixer sset Master 5%-")
+                         -- Set up our sound buttons
+                        
+                         , ("M-n", windows W.focusUp)
+                         , ("M-p", windows W.focusDown)
+                         , ("M-o", toggleWS)
+                         ]
 
 myStartupHook = do
 --	spawn "xsetroot -cursor_name left_ptr"
