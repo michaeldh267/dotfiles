0fc6aa005b0aa51a93b10de7ae3e7f1717bd29fa
Myk267 <mykyoyo@gmail.com>
2017-09-08 21:54:16 -0700

    update

--- install-vim.bash
+++ install-vim.bash
@@ -6,17 +6,22 @@
 #set -o pipefail # do not mask pipeline errors
 #set -x # debug mode 
 #IFS=$'\n\t'
+
 install-vim() {
-	( # subshell
-	cd ~/dotfiles
-	xstow vim
-	mkdir -pv ~/.vim/{swp,backup}
-	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-		curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-	cd ~/.vim/bundle && \
-		git clone https://github.com/sjl/badwolf
-	cd ~/.vim/bundle && \
-		git clone https://github.com/vim-syntastic/syntastic
-	)
+cd ~/dotfiles
+xstow vim
+mkdir -pv ~/.vim/{swp,backup,undo}
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle && \
+	git clone https://github.com/sjl/badwolf
+cd ~/.vim/bundle && \
+	git clone https://github.com/vim-syntastic/syntastic
+cd ~/.vim/bundle && \
+	git clone git://github.com/tpope/vim-fireplace.git
+cd ~/.vim/bundle && \
+	git clone git://github.com/tpope/vim-classpath.git
+cd ~/.vim/bundle && \
+	git clone git://github.com/guns/vim-clojure-static.git
 }
 install-vim
