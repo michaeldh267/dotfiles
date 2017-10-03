3f39aac7c914594986850095a0e856037d5899b1
Myk267 <mykyoyo@gmail.com>
2017-10-02 18:16:16 -0700

    disable some plugins

--- install-vim.bash
+++ install-vim.bash
@@ -16,11 +16,11 @@ cd ~/.vim/bundle && \
 	git clone https://github.com/sjl/badwolf
 cd ~/.vim/bundle && \
 	git clone https://github.com/vim-syntastic/syntastic
-cd ~/.vim/bundle && \
-	git clone git://github.com/tpope/vim-fireplace.git
-cd ~/.vim/bundle && \
-	git clone git://github.com/tpope/vim-classpath.git
-cd ~/.vim/bundle && \
-	git clone git://github.com/guns/vim-clojure-static.git
+#cd ~/.vim/bundle && \
+#	git clone git://github.com/tpope/vim-fireplace.git
+#cd ~/.vim/bundle && \
+#	git clone git://github.com/tpope/vim-classpath.git
+#cd ~/.vim/bundle && \
+#	git clone git://github.com/guns/vim-clojure-static.git
 }
 install-vim
