3959e20decb4e4a6fc788b8dcdf3416671b6e671
Myk267 <mykyoyo@gmail.com>
2018-02-04 08:09:07 -0800

    add floating windows to xmonad

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -3,22 +3,26 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders (smartBorders)
 
-main = xmonad desktopConfig
-	{ 
+main = xmonad desktopConfig { 
 	terminal = myTerminal,
-	modMask = mod4Mask,
-	borderWidth = 1,
-	focusedBorderColor = "#00ff00",
-	normalBorderColor = "#ff00ff",
-	layoutHook = smartBorders $ myLayout
- }
+        modMask = mod4Mask,
+        borderWidth = 1,
+        focusedBorderColor = "#00ff00",
+        normalBorderColor = "#ff00ff",
+        layoutHook = smartBorders $ myLayout,
+	manageHook = myManageHook <+> manageHook desktopConfig
+}
 
 myTerminal = "terminator"
 
 myLayout = tiled ||| Mirror tiled ||| Full
-    where
-        tiled = Tall nmaster delta ratio
-	  where
-	     nmaster = 1
-	     delta = 3/100
-	     ratio - 3/5
+  where
+    tiled = Tall nmaster delta ratio
+      where
+        nmaster = 1
+        delta = 3/100
+        ratio = 3/5
+
+myManageHook = composeAll
+	[ className =? "Yakuake" --> doFloat,
+	manageDocks ]
